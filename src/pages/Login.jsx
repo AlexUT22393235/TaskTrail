a3a4c12926d7c66ec30fe7eb18ef284244a6bd3b
AlexUT22393235 <22393235@utcancun.edu.mx>
2024-01-27 21:29:01 -0500

    Avance de login. Alex

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import logo from '../assets/logo.jpeg'
 
 function Login() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-screen">
       {/* Sección Izquierda */}
-      <div className="flex-1 bg-blue-500 p-8">
-        <div className="max-w-md mx-auto">
+      <div className="flex items-center justify-center h-screen w-2/5 bg-white p-8">
+        <div className="max-w-md mx-auto border-2 rounded">
           {/* Logo */}
-          <img src="/path/to/logo.png" alt="Logo" className="mb-4" />
+          <img src={logo} alt="Logo" className="mb-4 pt-28 pr-28 pl-28" />
+          <h2 className='text-2xl text-center'>INICIO SESIÓN</h2>
 
           {/* Formulario de Inicio de Sesión */}
           <form>
             <div className="mb-4">
               <label className="block text-white text-sm font-semibold mb-2" htmlFor="username">
-                Nombre de Usuario
+                USUARIO
               </label>
               <input
                 type="text"
                 id="username"
                 name="username"
-                className="w-full p-2 border border-white rounded"
-                placeholder="Ingrese su nombre de usuario"
+                className="w-full p-2 border-2 border-gray rounded"
+                placeholder="USUARIO"
               />
             </div>
 
@@ -32,14 +34,14 @@ function Login() {
                 type="password"
                 id="password"
                 name="password"
-                className="w-full p-2 border border-white rounded"
+                className="w-full p-2 border-2 border-gray rounded"
                 placeholder="Ingrese su contraseña"
               />
             </div>
 
             <button
               type="submit"
-              className="w-full bg-white text-blue-500 p-2 rounded hover:bg-blue-700 hover:text-white"
+              className="w-full bg-cyan-800 text-white p-2 rounded"
             >
               Ingresar
             </button>
@@ -48,7 +50,7 @@ function Login() {
       </div>
 
       {/* Sección Derecha */}
-      <div className="flex-1 bg-gray-300">
+      <div className="flex-auto w-3/5 bg-gray-300">
         <img src="https://images.unsplash.com/photo-1650429960273-8cb6ceefe98f?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Imagen" className="object-cover w-full h-full" />
       </div>
     </div>
