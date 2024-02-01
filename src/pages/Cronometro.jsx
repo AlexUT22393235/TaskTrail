d81f2658cf15ff7b96aab5fdc6324d32d15a0c66
AlexUT22393235 <22393235@utcancun.edu.mx>
2024-02-01 01:14:21 -0500

    Cronómetro de trabajo. Alex

--- src/pages/Cronometro.jsx
+++ src/pages/Cronometro.jsx
@@ -1,25 +1,72 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
 import { Link } from 'react-router-dom';
-import { IoArrowBack } from "react-icons/io5";
+import { IoArrowBack } from 'react-icons/io5';
 
 function Cronometro() {
-    return (
-        <>
-        <Header></Header>
-        <Link to="/Secciones">
-            <IoArrowBack size="3rem"/>
-            </Link>
-        <h1 className="font-semibold text-4xl text-center">CRONOMETRO DE TRABAJO</h1>
-        
-            <p className="text-center p-10 text-2xl">Timepo Transcurrido</p>
-            <p className="text-center p-10 text-4xl">00:00:00</p>
-        <div className="flex justify-center items-center ">
-            <button className="bg-blue-900 text-white p-3 hover:bg-blue-700 rounded-lg mr-10">Iniciar Trabajo</button>
-            <button className="bg-blue-400 text-white p-3 hover:bg-blue-300 rounded-lg">Terminar Trabajo</button>
-        </div>
-        </>
-    )
+  const [tiempoTranscurrido, setTiempoTranscurrido] = useState(0);
+  const [cronometroActivado, setCronometroActivado] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (cronometroActivado) {
+      interval = setInterval(() => {
+        setTiempoTranscurrido((prevTiempo) => prevTiempo + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [cronometroActivado]);
+
+  const formatearTiempo = (segundos) => {
+    const horas = Math.floor(segundos / 3600);
+    const minutos = Math.floor((segundos % 3600) / 60);
+    const segundosRestantes = segundos % 60;
+    
+    
+
+    const formatoHora = (valor) => (valor < 10 ? `0${valor}` : valor);
+
+    return `${formatoHora(horas)}:${formatoHora(minutos)}:${formatoHora(segundosRestantes)}`;
+  };
+
+  const handleIniciar = () => {
+    setCronometroActivado(true);
+  };
+
+  const handleParar = () => {
+    setCronometroActivado(false);
+  };
+
+  return (
+    <>
+      <Header />
+      <Link to="/Secciones">
+        <IoArrowBack size="3rem" />
+      </Link>
+      <h1 className="font-semibold text-5xl text-center">CRONÓMETRO DE TRABAJO</h1>
+
+      <p className="text-center m-10 p-10 mb-1 text-5xl">Tiempo transcurrido:</p>
+      <p className="text-center p-10 text-8xl">{formatearTiempo(tiempoTranscurrido)}</p>
+      <div className="flex justify-center items-center space-x-24 mt-32">
+        <button
+          className="bg-blue-900 text-white p-3 hover:bg-blue-700 rounded-lg mr-10 w-48 h-16"
+          onClick={handleIniciar}
+          disabled={cronometroActivado}
+        >
+          Iniciar Trabajo
+        </button>
+        <button
+          className="bg-blue-400 text-white p-3 hover:bg-blue-300 rounded-lg w-48 h-16"
+          onClick={handleParar}
+          disabled={!cronometroActivado}
+        >
+          Terminar Trabajo
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Cronometro
+export default Cronometro;
